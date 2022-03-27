ccb48c6a7fd07c1314f5b8735c0d4769db006ba1
Berin Aniesh <berinaniesh@gmail.com>
2022-03-27 16:48:18 +0530

    update

--- src/calc_sum.rs
+++ src/calc_sum.rs
@@ -1,8 +1,11 @@
 use super::constants;
+use std::path::Path;
 use std::fs::File;
 use std::io::Read;
 use std::io::Error;
 
 pub fn calc(argument: &String) -> String {
+    let file_result: Result<File, Error> = File::open(Path::new(argument));
+    
     "Hello".to_string()
 }
--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ mod calc_sum;
 mod constants;
 
 fn main() {
-    let arguments = std::env::args().skip(1);
+    let arguments = std::env::args().skip(1); //skip first argument cuz it's the path of the executable itself. 
     for argument in arguments {
         let sha256sum: String = calc_sum::calc(&argument);
         println!("The SHA256 sum of {} is {}", argument, sha256sum);
